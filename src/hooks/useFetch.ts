4e95400f2c52e38866f4d9a9e3569cd61b5bd623
pedro henrique martins paiva <pedrofh3nix@gmail.com>
2021-02-14 21:18:42 -0300

    add a readme and refactoring package.json #2

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import useSWR from 'swr'
 
-export function useFetch<Data = string, Error = string>(url: string) {
+export function useFetch<Data = any, Error = any>(url: string) {
   const { data, error } = useSWR<Data, Error>(url, async url => {
     const response = await fetch(url)
     const data = await response.json()
